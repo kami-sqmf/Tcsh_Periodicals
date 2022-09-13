d3346ff824adfe7824edeb18f016d2ee0d349777
Kami-Sqmf <kamisqmf@gmail.com>
2022-09-13 21:24:04 +0200

    PreAlpha 2.0.7

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server"
 
+const secret = process.env.NEXTAUTH_SECRET;
+
 export async function middleware(req: NextRequest) {
     const url = req.nextUrl.clone()
     if (url.pathname == "/accounts") {
-        const session = await getToken({ req })
-        console.log(session)
+        const session = await getToken({ req, secret });
         if (!session) return NextResponse.redirect(new URL("/accounts/signin", req.url))
     }
 
